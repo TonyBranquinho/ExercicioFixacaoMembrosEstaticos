85236d5bc0bccbe22139e9ca999d86095312673b
Tony <antony.branquinho@gmail.com>
2024-01-15 15:49:43 -0300

    Dificl, dai fiz meio q do meu geito, mas ta parecido ate

---
+++ ConversorDeMoedas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoMembrosEstaticos {
+    internal class ConversorDeMoedas {
+
+       public static double ValorEmReais(double Cotacao, double QuantidadeDolares) {
+            return Cotacao * (QuantidadeDolares * 1.06);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ExercicioFixacaoMembrosEstaticos;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Qual é a cotaçao do dolar? ");
+            double Cotacao = double.Parse(Console.ReadLine(), CI);
+            
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double QuantidadeDolares = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine(ConversorDeMoedas.ValorEmReais(Cotacao, QuantidadeDolares).ToString("F2", CI));
+        }
+    }
+}
